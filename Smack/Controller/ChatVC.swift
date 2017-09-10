7cafb45dbfd2abe278944cda574379893be8e006
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-11 04:02:55 +0900

    Added/Fixed SWReveal bug

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,20 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Stanley Pan on 11/09/2017.
+//  Copyright © 2017 Stanley Pan. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    //  MARK: Outlets
+    @IBOutlet weak var menuButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+}
