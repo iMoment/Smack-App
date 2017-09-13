f00fe637980f60adb5fd710edd1c119288b38ff7
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-14 01:58:51 +0900

    Added actions for CreateAccountVC

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -10,13 +10,59 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //  MARK: Outlets
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     @IBAction func closeButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
+    @IBAction func chooseAvatarPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func generateBackgroundColorPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func createAccountPressed(_ sender: UIButton) {
+        guard let email = emailTextField.text, emailTextField.text != "" else { return }
+        guard let password = passwordTextField.text, passwordTextField.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            if success {
+                print("We registered a user.")
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
