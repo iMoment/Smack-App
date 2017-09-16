daed322304e8f3cf697136e815b8781ec247fca8
Stanley Pan <stanleypan.egre@gmail.com>
2017-09-16 20:11:47 +0900

    LoginVC more fleshed out.

--- Smack/Controller/LoginVC.swift
+++ Smack/Controller/LoginVC.swift
@@ -9,20 +9,58 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
+    //  MARK: Outlets
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupView()
+    }
+    
+    func setupView() {
+        activityIndicator.isHidden = true
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: "username", attributes: [.foregroundColor: smackPurplePlaceholder])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "password", attributes: [.foregroundColor: smackPurplePlaceholder])
     }
     
     @IBAction func closeButtonPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let email = usernameTextField.text, usernameTextField.text != "" else { return }
+        guard let password = passwordTextField.text, passwordTextField.text != "" else { return }
+        
+        AuthService.instance.logInUser(email: email, password: password) { (success) in
+            if success {
+                AuthService.instance.retrieveUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIFICATION_USER_DATA_DID_CHANGE, object: nil)
+                        self.activityIndicator.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+        }
+    }
+    
     @IBAction func createAccountButtonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: TO_CREATE_ACCOUNT, sender: nil)
     }
-    
-    
-    
 }
+
+
+
+
+
+
+
+
+
+
--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -15,6 +15,7 @@ let BASE_URL = "https://smackitysmack.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 
 //  MARK: Colors
 let smackPurplePlaceholder = #colorLiteral(red: 0.3266413212, green: 0.4215201139, blue: 0.7752227187, alpha: 0.504682149)
@@ -35,5 +36,10 @@ let USER_EMAIL = "userEmail"
 
 //  MARK: Headers
 let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
+
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.authToken)",
     "Content-Type": "application/json; charset=utf-8"
 ]
